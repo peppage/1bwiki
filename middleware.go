0095162c223155a74ba58f20bf3e4a1ecdd93f09
Michael Peppler <mpeppler@carnegiehall.org>
2015-11-20 14:21:59 -0500

    :bug: only redirect GET requests

--- middleware.go
+++ middleware.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strings"
 
@@ -11,15 +10,17 @@ import (
 func fixUrl() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c *echo.Context) error {
-			u := strings.Trim(c.Request().URL.String(), "/")
-			f := string(u[0])
-			if f == strings.ToLower(f) {
-				newUrl := strings.Replace(strings.ToUpper(f)+string(u[1:]), "%20", "_", -1)
-				return c.Redirect(http.StatusMovedPermanently, newUrl)
-			}
-			if strings.Contains(u, "%20") {
-				newUrl := strings.Replace(u, "%20", "_", -1)
-				return c.Redirect(http.StatusMovedPermanently, newUrl)
+			if c.Request().Method == "GET" {
+				u := strings.Trim(c.Request().URL.String(), "/")
+				f := string(u[0])
+				if f == strings.ToLower(f) {
+					newUrl := strings.Replace(strings.ToUpper(f)+string(u[1:]), "%20", "_", -1)
+					return c.Redirect(http.StatusMovedPermanently, newUrl)
+				}
+				if strings.Contains(u, "%20") {
+					newUrl := strings.Replace(u, "%20", "_", -1)
+					return c.Redirect(http.StatusMovedPermanently, newUrl)
+				}
 			}
 			return next(c)
 		}
