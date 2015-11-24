0d174fc1ed1b2318d02c89a5d92ff9f10352ba54
Michael Peppler <mpeppler@carnegiehall.org>
2015-11-24 17:32:57 -0500

    move some javascript to site.js

---
+++ static/site.js
@@ -0,0 +1,49 @@
+// Because highlight.js is a bit awkward at times
+var languageOverrides = {
+  js: 'javascript',
+  html: 'xml'
+};
+
+emojify.setConfig({ img_dir: '/Static/emoji' });
+
+var md = markdownit({
+  html: true,
+  highlight: function(code, lang){
+    if(languageOverrides[lang]) lang = languageOverrides[lang];
+    if(lang && hljs.getLanguage(lang)){
+      try {
+        return hljs.highlight(lang, code).value;
+      }catch(e){}
+    }
+    return '';
+  }
+})
+.use(markdownitFootnote);
+
+function update(e){
+  setOutput(e.getValue());
+}
+
+function setOutput(val){
+  val = val.replace(/<equation>((.*?\n)*?.*?)<\/equation>/ig, function(a, b){
+    return '<img src="http://latex.codecogs.com/png.latex?' + encodeURIComponent(b) + '" />';
+  });
+
+  var out = document.getElementById('out');
+  var old = out.cloneNode(true);
+  out.innerHTML = md.render(val);
+  emojify.run(out);
+
+  var allold = old.getElementsByTagName("*");
+  if (allold === undefined) return;
+
+  var allnew = out.getElementsByTagName("*");
+  if (allnew === undefined) return;
+
+  for (var i = 0, max = Math.min(allold.length, allnew.length); i < max; i++) {
+    if (!allold[i].isEqualNode(allnew[i])) {
+      out.scrollTop = allnew[i].offsetTop;
+      return;
+    }
+  }
+}
