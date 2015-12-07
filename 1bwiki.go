081f3029c77b972adbd748649134f4fba0b143b1
Michael Peppler <peppage@gmail.com>
2015-12-07 01:16:02 -0500

    :hammer: mv db creating to model

--- 1bwiki.go
+++ 1bwiki.go
@@ -123,17 +123,6 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	// Convert to transaction
-	db.Exec(`create table if not exists text (id integer primary KEY, text blob)`)
-	db.Exec(`create table if not exists revision (id integer primary key,
-			pagetitle text, textid integer, comment text, userid int,
-			usertext text, minor integer, deleted integer, len integer,
-			parentid integer, timestamp integer, lendiff integer)`)
-	db.Exec(`create table if not exists page (title text,
-			namespace text, nicetitle text, redirect integer, revisionid integer,
-			len integer, PRIMARY KEY(title, namespace))`)
-	db.Exec(`CREATE TABLE IF NOT EXISTS user (id integer PRIMARY KEY, name text,
-			realname text text default "", password text, registration int, UNIQUE(id, name))`)
 }
 
 func main() {
