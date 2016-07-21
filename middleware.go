2d722428cc03dc73e09e15c53611c9b9bc4fcc70
Michael Peppler <mpeppler@carnegiehall.org>
2016-07-21 16:00:26 -0400

    remove final bits of echo

--- middleware.go
+++ middleware.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"net/http"
-	"time"
 
 	mdl "1bwiki/model"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/kataras/iris"
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/engine/standard"
 )
 
 type sessionMiddleware struct{}
@@ -54,32 +51,4 @@ func (s *adminMiddleware) Serve(c *iris.Context) {
 	}
 	c.Error("Must be admin ", http.StatusUnauthorized)
 	return
-}
-
-func serverLogger() echo.MiddlewareFunc {
-	return func(h echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			req := c.Request().(*standard.Request).Request
-			res := c.Response().(*standard.Response)
-
-			start := time.Now()
-			if err := h(c); err != nil {
-				c.Error(err)
-			}
-			stop := time.Now()
-			method := req.Method
-			path := req.URL.Path
-			if path == "" {
-				path = "/"
-			}
-
-			log.WithFields(log.Fields{
-				"method": method,
-				"path":   path,
-				"code":   res.Status(),
-				"time":   stop.Sub(start).String(),
-			}).Debug()
-			return nil
-		}
-	}
 }
